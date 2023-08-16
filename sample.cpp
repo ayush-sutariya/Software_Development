294e95fa2e25142389ca353c4e58d434603410d2
The Demolisher <demolisher@demolisher.demolisher>
2023-08-16 16:27:59 -0700

    Added Sample.cpp -16/08/2023 -16.27

--- sample.cpp
+++ sample.cpp
@@ -2,8 +2,48 @@
 
 using namespace std;
 
+class Hero {
+    private:
+    string name = "Tom";
+
+    void print_name(){
+        cout << name << endl;
+    }
+
+    public:
+     int health;
+     string level;
+
+     int get_health(){
+        return  health; 
+     }
+
+     string get_name(){
+        return name;
+     }
+
+     void set_health(int h){
+        health = h;
+     };
+
+     void set_name (string s) {
+        name = s;
+     };
+
+};
+
 int main(){
-    cout << "Hello World";
+
+    Hero h1;
+
+    h1.health = 70;
+    h1.level = "A";
+
+    cout << "Size: "<<sizeof(h1)<<endl;
+
+    cout << "Name: "<< h1.get_name()<<endl;
+
+    cout << "Health is: "<<h1.health << endl;
 
     return 0;
 }
